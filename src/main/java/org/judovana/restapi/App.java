ded09a5e0e0e0964b4ee3029467c782878bcd324
Jiri <jvanek@redhat.com>
2019-02-26 09:05:15 +0100

    Main pooling made testable

--- src/main/java/org/judovana/restapi/App.java
+++ src/main/java/org/judovana/restapi/App.java
@@ -1,31 +1,44 @@
 package org.judovana.restapi;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.MalformedURLException;
-import java.util.Arrays;
 import java.util.Date;
 import org.judovana.restapi.beans.Loan;
 import org.judovana.restapi.connection.Provider;
 
 public class App {
-    
+
     public static void main(String[] args) throws MalformedURLException, IOException, InterruptedException {
         if (args.length != 1) {
             throw new RuntimeException("Expected exactly one argument - URL of api");
         }
+        new App(args[0], 5, System.out).pool();
+    }
+
+    public void pool() throws InterruptedException, IOException {
         Db db = new Db();
         //exercise - 5 minutes; but check last 6 minutes and check against db as there oculd be loan in the few milisicends the request took
         while (true) {
-            Provider provider = Provider.create(args[0], 6);
+            Provider provider = Provider.create(baseUrl, delay + 1);
             Loan[] loans = provider.readLoans();
             System.out.println(new Date());
             for (Loan loan : loans) {
                 if (!db.contains(loan)) {
                     db.add(loan);
-                    System.out.println(loan);
+                    out.println(loan);
                 }
             }
-            Thread.sleep(5 * 60 * 1000);
+            Thread.sleep(delay * 60 * 1000);
         }
+    }
+    private final String baseUrl;
+    private final int delay;
+    private final PrintStream out;
+
+    private App(String arg, int delayIniMinutes, PrintStream out) {
+        this.baseUrl = arg;
+        this.delay = delayIniMinutes;
+        this.out = out;
     }
 }
